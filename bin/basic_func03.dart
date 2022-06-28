778bade061fa1b93d3ac2cedfc7e13d5f8d00a5e
sardor <ruziqulovsardor3@gmail.com>
2022-06-28 14:08:11 +0500

    code

--- bin/basic_func03.dart
+++ bin/basic_func03.dart
@@ -1,4 +1,15 @@
 // Create a function called func.
 
 // Return the value "codeschooluz"
-void main() {}
+//  Return the value "Hello World"
+String func(){
+    return 'codeschooluz';
+}
+
+
+
+void main() {
+print(func());
+
+
+}
